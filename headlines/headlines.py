ab7ef1714b228556a39b4e6c19cdaa1c1a6a6f1f
mohamedhany2002 <goldenhany94@gmail.com>
2020-10-06 01:17:45 +0200

    seecond

--- headlines/headlines.py
+++ headlines/headlines.py
@@ -1,9 +1,18 @@
 from flask import Flask
 app = Flask(__name__)
-
+import feedparser
+RSS_FEEDS = {
+            'bbc':'http://feeds.bbci.co.uk/news/rss.xml',
+            'cnn':'http://rss.cnn.com/rss/edition.rss',
+            'fox':'http://feeds.foxnews.com/foxnews/latest',
+            'iol':'http://www.iol.co.za/cmlink/1.640'
+            }
 
 @app.route('/')
-def get_news():
-    return 'no news to display'
+@app.route('/<publication>')
+def get_news(publication='bbc'):
+    feed = feedparser.parse(RSS_FEEDS[publication])
+    first_item = feed['entries'][0]
+    return first_item.get("title")
 if __name__=='__main':
     app.run(debug=True)
