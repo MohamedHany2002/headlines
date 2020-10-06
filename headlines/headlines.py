7cc7481aa69c3d9dd2ea9fee1080176de0aa5a35
mohamedhany2002 <goldenhany94@gmail.com>
2020-10-06 03:49:28 +0200

    ensuring

--- headlines/headlines.py
+++ headlines/headlines.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 app = Flask(__name__)
 import feedparser
 RSS_FEEDS = {
@@ -14,6 +14,17 @@ def get_news(publication='bbc'):
     feed = feedparser.parse(RSS_FEEDS[publication])
     items = feed['entries']
     return render_template('home.html',items=items)
+
+@app.route('/search')
+def search():
+    query = request.args.get('q')
+    if not query.lower() in RSS_FEEDS:
+        query='bbc'
+    feed = feedparser.parse(RSS_FEEDS[query.lower()])
+    items = feed['entries']
+    return render_template('home.html',items=items,searched=query)
+
+
 
 if __name__=='__main':
     app.run(debug=True)
